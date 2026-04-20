c60f53d617c10637c8a5cbba3095e113585d0083
Developer <developer@example.com>
2026-04-21 01:31:10 +0800

    Update .gitignore: exclude deploy/, run_cmake.sh, .qmake.stash

--- deploy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 启动脚本
-
-cd "$(dirname "$0")"
-export QT_QPA_PLATFORM=linuxfb
-./EnviroTrend_demo
--- run_cmake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set CMake path
-export PATH="/home/embedfire/QtProject/EnviroTrend_demo-main/cmake-3.28.0-linux-x86_64/bin:$PATH"
-
-# Run CMake command with provided arguments
-cmake "$@"
